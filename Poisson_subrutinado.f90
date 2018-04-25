15e31fc0dcb729d6be8cfb3865aa3ec18da0b7a1
javiyo <javierestegracia@gmail.com>
2018-04-25 19:09:44 +0200

    Update Poisson_subrutinado.f90

--- Poisson_subrutinado.f90
+++ Poisson_subrutinado.f90
@@ -5,8 +5,9 @@ implicit none
 integer :: i,j,k,cont,np,np2
 real(8),allocatable :: m(:,:),u(:),r(:),uapro(:,:)
 real(8) :: a, b,h,alpha,beta
-real(8),parameter :: pi=dacos(-1.0d0)
-	np=20
+real(8),parameter :: pi=dacos(-1.0d0),tolconj=1.0d-6
+integer,parameter :: iteraconj=100
+	np=50
 	np2=np**2 !np elevado al cuadrado, ya que np significa el numero de puntos en una dimension
 	alpha=0
 	beta=1
@@ -17,12 +18,31 @@ real(8),parameter :: pi=dacos(-1.0d0)
 
 	call invoca_matvect(m,r,h,np)
 	call rverdadero(uapro,h,a,np)
-	call grad_conj(m,r,u,1.0d-6,100)
+	call grad_conj(m,r,u,tolconj,iteraconj)
 
 	do i=1,np2
 	cont =cont+1
 	print *,  'Valores de u', u(i),'/////', cont
 	end do	
+	
+	open(unit=12, file='resultado_exacto(aprox).txt')
+	write(12,*) 'Numero puntos:', np
+	do j=1,np
+		do i=1,np
+		write(12,*) uapro(j,i)
+		end do
+	end do
+	close(11)
+
+	open(unit=13, file='solucion_numerica.txt')
+	write(13,*) 'Numero puntos:', np
+	write(13,*) 'Numero de iteraciones gradiente conjugado', iteraconj
+	write(13,*) 'Tolerancia gradiente conjugado', tolconj
+	do i=1,np2
+	write(13,*) u(i)
+	end do
+	close(11)
+
 	cont=0
 	do j=1,np
 		do i=1,np
@@ -31,10 +51,10 @@ real(8),parameter :: pi=dacos(-1.0d0)
 		end do
 	end do
 
-	open(unit=11, file='matriz.txt')
-	do i=1,np2
-	write(11,"(25I2)")int(m(i,:))
-	end do
-	close(11)
+	!open(unit=11, file='matriz.txt')
+	!do i=1,np2
+	!write(11,"(25I2)")int(m(i,:))
+	!end do
+	!close(11)
 	
 end program main
