fab527cb3fe00ff70197bbc11bec820498d89408
javiyo <javierestegracia@gmail.com>
2018-04-28 11:41:52 +0200

    Update mod_sistemas.f90

--- mod_sistemas.f90
+++ mod_sistemas.f90
@@ -42,4 +42,33 @@ contains
                     norma=sqrt(norma)
 end function norma
 
+subroutine gausspivote(a,b,x)
+real(8), intent(out) :: x(:)
+real(8) :: suma, f, a(:,:), b(:)
+real(8),dimension(size(b),size(b)) :: m
+real(8),dimension(size(b)) :: h
+integer :: k,j,i,fila
+m=a
+h=b
+n=size(b)
+do k=1,n-1! etapas de e l i m i n a c i o n
+	fila=maxloc(abs(a(k:n,k)),1)+k-1! pivote
+	a([k,fila],k:n) = a([fila,k],k:n)! interc .
+	b([k,fila]) = b([fila,k])! interc .
+		do i=k+1,n! f i l a s a eliminar
+		f=a(i,k)/a(k,k)
+		a(i,k+1:n) = a(i,k+1:n) - a(k,k+1:n)*f
+		b(i)=b(i)-b(k)*f
+		end do
+end do
+do k= n,1,-1
+	suma =0.0
+	suma = dot_product(a(k,k+1:n),x(k+1:n))
+	x(k) = ( b(k) - suma ) / a(k,k)
+end do
+a=m
+b=h
+end subroutine gausspivote
+
+
 end module sistemas
